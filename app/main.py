c19fa0d3b68801768946237c74e7b302b95974a1
Lucas Grenier <luc@sgrenier.com>
2025-05-03 16:33:14 -0400

    Add constellation addition endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import os
+import json
 
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import SQLModel, create_engine, Session, select
 
 from .models import *
 
@@ -25,7 +26,28 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 
 @app.get("/")
-def root():
+async def root():
     with open("../templates/index.html", "r") as fp:
         return fp.read()
+
+
+@app.post("/add-constellation")
+async def add_constellation(
+        scientific_name: str = Form(...),
+        informal_name: str = Form(...),
+        star_coords: str = Form(...)
+    ):
+    star_coords = json.dumps([s.strip() for s in star_coords.split(",")])
+    constellation = Constellation(scientific_name=scientific_name, informal_name=informal_name, star_coords=star_coords)
+    scientific_checker = select(Constellation).where(Constellation.scientific_name == scientific_name)
+    informal_checker = select(Constellation).where(Constellation.informal_name == informal_name)
+    with Session(engine) as session:
+        if session.exec(scientific_checker).all():
+            return "That scientific name is already in the database. Try another!"
+        if session.exec(informal_checker).all():
+            return "That informal name is already in the database. Try another!"
+        session.add(constellation)
+        session.commit()
+        session.refresh(constellation)
+    return f"{scientific_name} has been added to our database!"
 
--- app/models.py
+++ app/models.py
@@ -3,6 +3,6 @@ from sqlmodel import SQLModel, Field
 class Constellation(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     scientific_name: str
-    general_names: str
+    informal_name: str
     star_coords: str
 
